bde4ce23d6119371a238fe317ded43e1df011c0a
Santo Pfingsten <santo@lusito.info>
2015-05-05 18:14:20 +0200

    Linux support for changing working directory

--- Source/Utils/SystemUtils.cpp
+++ Source/Utils/SystemUtils.cpp
@@ -5,36 +5,32 @@ using std::string;
 #ifdef WIN32
 	#include <windows.h>
 	#include <direct.h>
+	#define PATH_MAX MAX_PATH
 #else
 	#include <unistd.h>
+	#include <linux/limits.h>
 #endif
 
 namespace SystemUtils {
-#ifdef WIN32
 	string getExecutableDirectory()
 	{
-		char buffer[MAX_PATH];
-		GetModuleFileName(NULL, buffer, MAX_PATH);
+		char buffer[PATH_MAX + 1] = { 0 };
+#ifdef WIN32
+		GetModuleFileName(NULL, buffer, PATH_MAX);
+#else
+		char linkPath[20];
+		snprintf(linkPath, 20, "/proc/%d/exe", getpid());
+		readlink(linkPath, buffer, PATH_MAX);
+#endif
 		string::size_type pos = string(buffer).find_last_of("\\/");
 		return string(buffer).substr(0, pos);
 	}
 	void setCwd(const string &dir)
 	{
+#ifdef WIN32
 		_chdir(dir.c_str());
-	}
 #else
-	string getExecutableDirectory()
-	{
-		char arg1[20];
-		char exepath[PATH_MAX + 1] = { 0 };
-
-		sprintf(arg1, "/proc/%d/exe", getpid());
-		readlink(arg1, exepath, 1024);
-		return string(exepath);
-	}
-	void setCwd(const string &dir)
-	{
 		chdir(dir.c_str());
-	}
 #endif
+	}
 }
